b3fbb4b68dbda010ee2bad3ccd01d6fa8d6a0815
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-13 00:15:08 +0000

    first generate commit

--- src/app.js
+++ src/app.js
@@ -4,15 +4,42 @@ import "./style.css";
 
 import "./assets/img/4geeks.ico";
 
-window.onload = function generateRandomCard() {};
-let generateSuit = function generateSuit() {
-  const suits = ["hearts", "diamonds", "clubs", "spades"];
+window.onload = function generateRandomCard() {
+  let generateSuit = function generateSuit() {
+    const suits = [
+      "bi bi-suit-heart-fill",
+      "bi bi-suit-diamond-fill",
+      "bi bi-suit-club-fill",
+      "bi bi-suit-spade-fill"
+    ];
 
-  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
 
-  const suitElement = document.getElementsByClassName("icon");
+    const suitElement = document.getElementsByClassName("icon");
 
-  suitElement.classList.add(iconClasses[randomSuit]);
+    suitElement.classList.add(iconClasses[randomSuit]);
+  };
+
+  let generateCardNumber = function generateCardNumber() {
+    const cardNumbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    const randomCardNumber =
+      cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+
+    document.getElementsByClassName("number").textContent = randomCardNumber;
+  };
 };
-
-let generateCardNumber = function generateCardNumber() {};
