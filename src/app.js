61fed13c79e8d0de9fa78d115fa63bcdc4f6fd84
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-13 00:14:51 +0000

    first generate commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ import "./style.css";
 
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Generate your card!");
+window.onload = function generateRandomCard() {};
+let generateSuit = function generateSuit() {
+  const suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  const suitElement = document.getElementsByClassName("icon");
+
+  suitElement.classList.add(iconClasses[randomSuit]);
 };
+
+let generateCardNumber = function generateCardNumber() {};
